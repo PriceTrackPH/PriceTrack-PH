cf5ab705fcd951ffb2e577326d28b61261aa89af
Haoe! <41176431+GoodNames18@users.noreply.github.com>
2026-08-21 20:57:36 +0800

    Render full original page sections

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import SiteSections from "./SiteSections";
 import "./styles.css";
 import "./theme.css";
+import "./site-sections.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
+    <SiteSections />
   </StrictMode>,
 );
