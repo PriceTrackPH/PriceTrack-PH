447d25943593b515781cc9662d36d41e441f6e0f
Haoe! <41176431+GoodNames18@users.noreply.github.com>
2026-08-23 09:14:45 +0800

    Use JPG donation QR assets for GCash and Maya

--- src/SiteSections.tsx
+++ src/SiteSections.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import gcashQr from "./assets/donation-gcash-qr.png";
-import mayaQr from "./assets/donation-maya-qr.png";
+import gcashQr from "./assets/donation-gcash-qr.jpg";
+import mayaQr from "./assets/donation-maya-qr.jpg";
 import bankQr from "./assets/donation-bank-qr.png";
 
 function SiteSections() {
