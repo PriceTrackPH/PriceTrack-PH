8db3998ef43fe1a25501baa5fdaa4bd25b8afb6a
Haeo! <41176431+GoodNames18@users.noreply.github.com>
2026-08-28 21:03:03 +0800

    Stop disabling affiliate report button

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,6 @@ import "./variation-white-default.css";
 import "./report-lavender-fix.css";
 import "./title-fit";
 import "./variation-display-format";
-import "./affiliate-button";
 import "./mobile-donation-close.css";
 import "./mobile-contact-modal.css";
 
